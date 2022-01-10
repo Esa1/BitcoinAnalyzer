46920951993e391de85423ef80602f5cd266e2c8
Esa Torkkila <esa.torkkila@gmail.com>
2022-01-10 10:05:15 +0200

    prices arrays and prices dates analyzing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bestDayToSell = document.getElementById("BestDayToSell")*/
 var rangeStartDate = ""
 var bestDayToSell = ""
 window.onload = function() {
-var btn = document.getElementById("btn")
+var btnGetData = document.getElementById("GetData")
 
 /*var rangeStartDate = document.getElementById("RangeStartDate")
 const rangeEndDate = document.getElementById("RangeEndDate")
@@ -22,17 +22,50 @@ rangeStartDate = document.getElementById("BestDayToSell")
 bestDayToSell = document.getElementById("BestDayToSell")
 console.log(bestDayToSell);
 
-btn.addEventListener("click", Esa);
+btnGetData.addEventListener("click", RetrieveData);
 }
 /********************/
 /* Retrieve data from here: https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=1392577232&to=1422577232
 /********************/
-function Esa() {
+
+let rawData = ""
+function RetrieveData() {
     console.log("RetrieveData")
-//https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=1392577232&to=1422577232
-//console.log(btn)
-console.log(rangeStartDate)
-//console.log(rangeStartDate.valueAsNumber)
+    let url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=1392577232&to=1422577232";
+    //console.log(btn)
+    console.log(rangeStartDate)
+    //console.log(rangeStartDate.valueAsNumber)
+    $.getJSON(url, function(data, status) {
+//        console.log(status)
+//        console.log(data)
+//        rawData = data
+        prices = data.prices
+        // Handle prices array
+        processPrices(prices)
+
+    });
+
+function processPrices(prices) {
+        //prices[0] date
+        //prices[1] price
+        console.log("prices: ", prices)
+        console.log("prices 0: ", prices[0][0])
+        console.log(`prices.length: ${prices.length}`)
+//Loop through prices arrays
+
+        prices.forEach(element => {
+//            console.log(element[0])
+            d = new Date(element[0])
+            var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+            d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+            console.log(datestring)
+            console.log(element[1])
+        }); 
+
+/*
+    //    var datestring = d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+        */
+    }
 }
 
 
